ef0c331ce7a95c6fa6465242384ceaf39cea14d1
Wk-P <72795540+Wk-P@users.noreply.github.com>
2023-12-16 15:59:47 +0900

    Update add_node_label.sh

--- add_node_label.sh
+++ add_node_label.sh
@@ -5,16 +5,12 @@ do
     NODES_ID=$(echo 123321 | sudo -S docker node ls --format '{{ .ID}}')
     for node_id in $NODES_ID
     do
-        labels=$(echo 123321 | sudo -S docker node inspect --format '{{ json .Spec.Labels }}' "$node_id")
-        role=$(echo 123321 | sudo -S docker node inspect --format '{{ json .Spec.Role }}' "$node_id")
-        echo "$labels"
-        echo "$role"
-        if [ "$labels" == "{}" ] && [ "$role" == "worker" ]
+        labels=$(echo 123321 | sudo -S docker node inspect --format '{{ json .Spec.Labels }}' "$node_id" | jq -e 'length == 0')
+        role=$(echo 123321 | sudo -S docker node inspect --format '{{ json .Spec.Role }}' "$node_id" | jq -r .)
+
+        if [ "$labels" == "true" ] && [ "$role" == "worker" ]
         then
             echo 123321 | sudo -S docker node update --label-add 'node=worker' "$node_id"
-            
-        # else
-            # echo node id is $node_id
         fi
     done
     sleep 5
